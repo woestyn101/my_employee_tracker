d9679b15e615108c0eb8049c8a28eb6672fe8816
Marius Du Plooy <woestyn101@gmail.com>
2024-02-05 23:58:04 -0500

    process functions created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { main_menu_questions, add_department_question, add_role_questions, add_e
 import { departmentArray, roleArray, managerArray  } from './js/questions.js';
 import {db} from './js/db_connection.js';
 import { show_departments, get_Departments, get_Roles, get_Managers, show_employees, show_employee_roles, add_department, add_role, add_employee } from './js/display_db_functions.js';
-import { process_department } from './js/inquirer_prompt.js';
+
 
 
 console.log(chalk.bold.bgBlue("EMPLOYEE TRACKER"))
@@ -19,71 +19,25 @@ export function askQuestions(){
     inquirer
     .prompt(main_menu_questions)
     .then((answers) => {
-      // Use user feedback for... whatever!!
-      console.log(answers.choose_option);
+           console.log(answers.choose_option);
       if (answers.choose_option === "See all Departments") {
-          show_departments ();        
+              show_departments ();        
                    
       } else  if (answers.choose_option === "View employee roles") {
-        show_employee_roles ();       
+              show_employee_roles ();       
                  
     } else  if (answers.choose_option === "See all employees") {
-        show_employees ();       
+              show_employees ();       
                  
     } else  if (answers.choose_option === "Add a department") {
-
-      inquirer
-      .prompt(add_department_question)
-      .then((answers) => {
-        // Use user feedback for... whatever!!
-        add_department(answers.dept_name);
-        console.log("Department was added!");
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else went wrong
-        }
-      });   
+              process_dept();      
                  
     } else if(answers.choose_option === "Add a role"){
-
-        inquirer
-        .prompt(add_role_questions)
-        .then((answers) => {
-         
-        var convertoDeci = parseFloat(answers.the_salary);
-        var indexDept = (departmentArray.indexOf(answers.choose_department)) + 1
-        
-         add_role(answers.the_role,convertoDeci, indexDept );
-         
-        })
-        .catch((error) => {
-          if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-          } else {
-            // Something else went wrong
-          }
-        });   
+       
+                process_role();       
 
     } else if (answers.choose_option === "Add an employee") {
-                    inquirer
-            .prompt(add_employee_questions)
-            .then((answers) => {
-                var indexRoles = (roleArray.indexOf(answers.choose_therole)) + 1
-                var indexManager = (managerArray.indexOf(answers.the_manager)) + 1
-                            
-               
-                add_employee(answers.add_first_name, answers.add_last_name, indexRoles, indexManager)
-            })
-            .catch((error) => {
-                if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-                } else {
-                // Something else went wrong
-                }
-            });
+                 process_employee();
     }else {
         process.exit();
     }
@@ -91,9 +45,9 @@ export function askQuestions(){
     })
     .catch((error) => {
       if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
+        console.log(error);
       } else {
-        // Something else went wrong
+        console.log("New error");
       }
     });
   
@@ -101,8 +55,6 @@ export function askQuestions(){
 }
 
 
-
-// Query database
 
 // get departments from db to be used in inquirer as choice options
 get_Departments();
@@ -112,6 +64,61 @@ get_Roles();
 get_Managers();
 
 
+function process_dept(){
+
+  inquirer
+  .prompt(add_department_question)
+  .then((answers) => {
+   
+    add_department(answers.dept_name);
+    console.log("Department was added!");
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("error");
+    } else {
+        console.log(" new error");
+    }
+  });   
+
+}
+
+function process_role(){
+  inquirer
+  .prompt(add_role_questions)
+  .then((answers) => {
+   
+  var convertoDeci = parseFloat(answers.the_salary);
+  var indexDept = (departmentArray.indexOf(answers.choose_department)) + 1
+  
+   add_role(answers.the_role,convertoDeci, indexDept );
+   
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+     console.log(error);
+    } else {
+    console.log("new error");
+    }
+  });   
+}
 
 
-
+function process_employee(){
+  inquirer
+  .prompt(add_employee_questions)
+  .then((answers) => {
+      var indexRoles = (roleArray.indexOf(answers.choose_therole)) + 1
+      var indexManager = (managerArray.indexOf(answers.the_manager)) + 1
+                  
+     
+      add_employee(answers.add_first_name, answers.add_last_name, indexRoles, indexManager)
+  })
+  .catch((error) => {
+      if (error.isTtyError) {
+      console.log(error);
+      } else {
+      console.log("New error");
+      }
+  });
+}
